0e13bc3ad9af975e7cd860530551d9656509a0b5
Hagay Haut <hagayhaut@gmail.com>
2022-09-11 23:07:16 -0400

    import and configure monaco-editor and tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <p className="text-3xl font-bold underline">Hello</p>
+    </div>
+  );
 }
 
 export default App;
